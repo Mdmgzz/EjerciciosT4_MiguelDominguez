f9d7527d49a988918c4eeb2373ec19a0b7f434ee
mdominguezg <mdominguezg@106-06.info2>
2022-12-05 10:13:19 +0100

    commit

---
+++ T4_EjerciciosPrácticos/src/ejercicio04/Main.java
@@ -0,0 +1,32 @@
+package ejercicio04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// creamos scanner
+		Scanner sc=new Scanner(System.in);
+		//creamos objeto para llamar a las funciones
+				Operaciones objeto=new Operaciones();
+		
+		// preguntamos al usuario
+		
+		System.out.println("Introduzca el primer entero: ");
+		objeto.num1=sc.nextInt();
+		System.out.println("Introduzca el segundo entero: ");
+		objeto.num2=sc.nextInt();
+		
+		
+		// llamamos a las funciones 
+		System.out.println("La Suma de los numeros: " + objeto.suma());
+		System.out.println("La Resta de los numeros: " + objeto.resta());
+		System.out.println("La Multiplicación de los numeros: " + objeto.multiplicacion());
+		System.out.println("La división de los numeros: " + objeto.division());
+		
+		
+		//cerramos scanner
+		sc.close();
+	}
+
+}
---
+++ T4_EjerciciosPrácticos/src/ejercicio04/Operaciones.java
@@ -0,0 +1,33 @@
+package ejercicio04;
+
+public class Operaciones {
+	int num1;
+	int num2;
+	
+	public int suma () {
+		int resultado;
+		
+		resultado=num1+num2;
+		return resultado;
+	}
+	
+	public int resta () {
+		int resultado;
+		
+		resultado=num1-num2;
+		return resultado;
+	}
+	public int multiplicacion () {
+		int resultado;
+		
+		resultado=num1*num2;
+		return resultado;
+	}
+	public int division () {
+		int resultado;
+		
+		resultado=num1/num2;
+		return resultado;
+	}
+}
+
